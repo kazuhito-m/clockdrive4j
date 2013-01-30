6e01e5a472cfcb96d3009df00d70f6c195370664
kazuhito-m <kazuhito.sumpic@gmail.com>
2013-01-30 18:47:23 +0900

    test add for if condition coverage.

--- src/test/java/tk/bnbm/clockdrive4j/model/CloudTest.java
+++ src/test/java/tk/bnbm/clockdrive4j/model/CloudTest.java
@@ -3,6 +3,7 @@ package tk.bnbm.clockdrive4j.model;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
@@ -37,9 +38,25 @@ public class CloudTest {
 
 	@Test
 	public void 徐々に移動していく() {
+		// ※カバレッジ対策も含む。
 		Point2D.Double prev = (Double) sut.getPositions().get(0).clone();
 		sut.move(1);
 		Point2D.Double now = sut.getPositions().get(0);
 		assertThat(now, is(not(prev)));
+	}
+
+	@Test
+	public void 雲が座標中の下限を下回る場合() {
+		// Arrange 準備
+		sut.getPositions().clear(); // 座標データを全て削除
+		Point2D.Double p = new Point2D.Double(-512D, -512D); // 新しい座標データを作成。
+		sut.getPositions().add(p); // 一つだけ足す
+
+		// Act 実行
+		sut.move(+1D);
+
+		// Assert 検証
+		assertThat(p.getX(), is(not(0D)));	// ランダム要素を含むため、0でなければ
+		assertTrue(p.getY() >= 0D);
 	}
 }
