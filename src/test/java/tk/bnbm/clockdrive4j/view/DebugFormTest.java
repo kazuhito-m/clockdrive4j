482cf3d4040fe1190adac27019fda4b0d0df0b73
kazuhito-m <kazuhito.sumpic@gmail.com>
2013-02-06 14:27:06 +0900

    charenge to "UI auto test". add test class.

---
+++ src/test/java/tk/bnbm/clockdrive4j/view/DebugFormTest.java
@@ -0,0 +1,47 @@
+package tk.bnbm.clockdrive4j.view;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.*;
+import javafx.stage.Stage;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * DebugFormのテストクラス。<br>
+ *
+ * 「UIのカバレッジも向上できないか？」という観点から、実装してみたもの。<br>
+ * お試しなのであまり意味は無い。
+ *
+ * @author kazuhito_m
+ *
+ */
+public class DebugFormTest {
+
+	DebugForm sut; // テスト対象オブジェクト
+
+	@Before
+	public void setUp() throws Exception {
+		this.sut = new DebugForm(new MainView());
+	}
+
+	@Test
+	public final void コンストラクタにてデバッグ対象がセットされている() {
+		assertThat(sut.debugTarget, is(notNullValue()));
+	}
+
+	@Ignore
+	@Test
+	public final void startによりステージオブジェクトがセットされる() throws Exception {
+		// FIXME Mockでのテスト技法を期待。(現在勉強不足で使えず…）
+		// Arrange - 準備
+		Stage stage = new Stage();	//
+		// Act - 実行
+		sut.start(stage);
+		// Assert - 検証
+		assertThat(sut.self, is(stage));
+	}
+
+}
