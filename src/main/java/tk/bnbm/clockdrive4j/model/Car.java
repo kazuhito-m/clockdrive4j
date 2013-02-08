0183e8b3ea098fa4fc0e7c76aed20a31f91f24ac
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2013-02-08 11:31:05 +0900

    add comment and organized in this class.

--- src/main/java/tk/bnbm/clockdrive4j/model/Car.java
+++ src/main/java/tk/bnbm/clockdrive4j/model/Car.java
@@ -15,8 +15,14 @@ public class Car {
 
     // 定数群。
 
-    /** 計算用「2周分＋直角」角度。 */
-    private static final double TWO_LAPS_PLUS_ANGLE = 2.0D * 360.0D + 90.0D;
+    /** 計算用「全周」角度。 */
+    private static final double ALL_ANGLE = 360.0D;
+
+    /** 計算用「半周」角度。 */
+    private static final double HARF_ANGLE = 180.0D;
+
+    /** 計算用「垂直・90度」角度。 */
+    private static final double RIGHT_ANGLE = 90.0D;
 
     /** 計測範囲(秒) */
     private static final int MEASURING_RANGE_SEC = 5;
@@ -30,9 +36,9 @@ public class Car {
     private Road road;
 
     /**
-     * コンストラクタ。
-     * <br>引数にRoadインスタンスを受け取って、格納する。
-     *
+     * コンストラクタ。 <br>
+     * 引数にRoadインスタンスを受け取って、格納する。
+     * 
      * @param road 自身が走ることになる"道路"オブジェクト。
      */
     public Car(final Road r) {
@@ -41,7 +47,7 @@ public class Car {
 
     /**
      * 基準時刻を与える。
-     *
+     * 
      * @param t 基準時刻。
      */
     public void setTime(final Date time) {
@@ -50,17 +56,17 @@ public class Car {
 
     /**
      * 車を描くべき位置を取得する。
-     *
+     * 
      * @return 座標オブジェクト。
      */
     public Point2D.Double getPosition() {
-        return road.getRoadPosition(this.time);
+        return road.getPosition(this.time);
     }
 
     /**
-     * 車をの向き(描くべき角度を360度法)を取得する。
-     * <br>前後２点から滑らかに補完する。
-     *
+     * 車をの向き(描くべき角度を360度法)を取得する。 <br>
+     * 前後２点から滑らかに補完する。
+     * 
      * @return 角度(360度法)
      */
     public double getAngle() {
@@ -77,11 +83,12 @@ public class Car {
         Date after5Sec = c.getTime();
 
         // 座標算出。
-        Point2D.Double pA = road.getRoadPosition(before5Sec);
-        Point2D.Double pB = road.getRoadPosition(after5Sec);
-        double xDiff = (pA.getX() - pB.getX());
-        double yDiff = (pA.getY() - pB.getY());
-        double angle = Math.atan2(yDiff, xDiff);
-        return angle / PI / TWO_LAPS_PLUS_ANGLE;
+        Point2D.Double pA = road.getPosition(before5Sec);
+        Point2D.Double pB = road.getPosition(after5Sec);
+        double xDiff = pA.getX() - pB.getX();
+        double yDiff = pA.getY() - pB.getY();
+        double rad = Math.atan2(yDiff, xDiff);
+        double angle = rad * HARF_ANGLE / PI; // ラジアンを度に。
+        return (angle + RIGHT_ANGLE + ALL_ANGLE) % ALL_ANGLE; // 0～360度を越えないように。
     }
 }
